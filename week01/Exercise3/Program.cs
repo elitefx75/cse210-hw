b610032bc4aedb2221ff2b1b189c30a2f70a039b
elitefx75 <pbwire@byupathway.edu>
2025-11-04 14:05:36 +0300

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
