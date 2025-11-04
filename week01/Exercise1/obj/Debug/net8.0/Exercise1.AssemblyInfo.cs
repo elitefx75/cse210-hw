fb4050c909ffa5bb4b099858aec87e880cfa230c
elitefx75 <pbwire@byupathway.edu>
2025-11-04 12:44:36 +0300

    exercise 1

--- week01/Exercise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
+++ week01/Exercise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90306447adc0f9dfb0cd4fd4eddd97cd3a31ef3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8aef89801c255e653a3e9fac7606d45e533c19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
