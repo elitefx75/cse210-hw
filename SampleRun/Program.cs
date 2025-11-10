d576237968d0c28d769d339271cf27cc901f21c0
elitefx75 <pbwire@byupathway.edu>
2025-11-10 12:45:49 +0300

    resume added

---
+++ SampleRun/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var p = new Person();
+        p._givenName = "John";
+        p._familyName = "Doe";
+        p.ShowWesternName();
+        p.ShowEasternName();
+    }
+}
